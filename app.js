dea53d9bc99fd151946a368989a7129e70b39415
Javier Saiz <javiersaizcano@gmail.com>
2021-02-03 20:06:24 +0100

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var elem = document.querySelector(".main-carousel");
 var flkty = new Flickity(elem, {
   // options
+  wrapAround: true,
   autoPlay: 3000,
   prevNextButtons: false,
   pageDots: false,
