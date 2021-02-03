eb84fbb83c044c24f630375ae514f005d92b6715
Javier Saiz <javiersaizcano@gmail.com>
2021-02-03 20:03:14 +0100

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  // options
+  autoPlay: 3000,
+  prevNextButtons: false,
+  pageDots: false,
+  cellAlign: "left",
+  contain: true,
+});
