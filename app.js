7254ace9b6ec9f9daf58233d5e514fed70f1c627
Javier Saiz <javiersaizcano@gmail.com>
2021-02-04 15:01:42 +0100

    commit

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ var flkty = new Flickity(elem, {
   cellAlign: "left",
   contain: true,
 });
+
+var testimonials = document.querySelector(".testimonials-carousel");
+var flkty2 = new Flickity(testimonials, {
+  // options
+  wrapAround: true,
+  autoPlay: 4000,
+  prevNextButtons: false,
+  pageDots: true,
+  cellAlign: "left",
+  contain: true,
+});
